844c010b6989d9cdeacd0acd6d2e632e4d24005e
AlanTan <alan.tan@beacora.com>
2022-04-05 16:25:07 +0800

    add quickgrab and sushi go

--- quickGrab.py
+++ quickGrab.py
@@ -2,11 +2,16 @@ from PIL import ImageGrab
 import os
 import time
 
-print("Hello, World!")
+#Globals
+# -------------------------
+
+x_pad = 250
+y_pad = 276
  
 def screenGrab():
-    box = ()
-    im = ImageGrab.grab()
+    #box = (250,276,1047,876)
+    box = (x_pad+1, y_pad+1, x_pad+797, y_pad+600)
+    im = ImageGrab.grab(box)
     im.save(os.getcwd() + '\\full_snap__' + str(int(time.time())) +
 '.png', 'PNG')
  
